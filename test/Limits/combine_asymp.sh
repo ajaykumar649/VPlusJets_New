cd313b09bd2bcb400fdc7e579c3aa02459d8f98a
Phillip <pdudero@gmail.com>
2013-08-08 16:46:54 -0500

    make it more useful

--- test/Limits/combine_asymp.sh
+++ test/Limits/combine_asymp.sh
@@ -6,26 +6,30 @@ then
     exit
 fi
 
-#COMBINE_ARGS="-v0 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping --rMin=0.1 --rMax=5"
-COMBINE_ARGS="-v4 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping"
-
 #for mass in 170 180 190 200 250 300 350 400 450 500 550 600
 #for mass in 180 250 400
 for datacard in $*
 do
   DIR=`dirname $datacard`
+  COMBINE_ARGS="-v0 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping --rMin=0.1 --rMax=10"
+# COMBINE_ARGS="-v4 -n HWW2lnujjObs -M Asymptotic --minosAlgo stepping"
+
   file=`basename $datacard .txt`
   suffix=${file##datacard_}
   mass=`ls $datacard | egrep -o "M=[0-9]+" | egrep -o "[0-9]+"`
+  if [[ "$mass" -ne "" ]]
+  then
+      COMBINE_ARGS="$COMBINE_ARGS -m ${mass}"
+  fi
 #
 # "tee" used per recommendation from Giovanni, since it captures the stdout/stderr better than straight redirection;
 # the combine tool mucks with the stdout FILE descriptor...
 #
-  echo "combine $COMBINE_ARGS -m ${mass} $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null"
-  combine $COMBINE_ARGS -m ${mass} $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null
-#  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_${mass}.txt 2>&1 | tee ${DIR}/limit_${mass}.log >/dev/null
-#  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_mu2jet_${mass}.txt 2>&1 | tee ${DIR}/limit_mu2jet_${mass}.log >/dev/null
-#  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_withVBF_${mass}.txt 2>&1 | tee ${DIR}/limit_withVBF_${mass}.log >/dev/null
-#  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_5pctsyst_${mass}.txt 2>&1 | tee ${DIR}/limit_5pctsyst_${mass}.log >/dev/null
-#  combine $COMBINE_ARGS -m ${mass} ${DIR}/datacard_milwin_candc_2j_${mass}.txt 2>&1 | tee ${DIR}/limit_milwin_candc_2j_${mass}.log >/dev/null
+  echo "combine $COMBINE_ARGS $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null"
+  combine $COMBINE_ARGS $datacard 2>&1 | tee ${DIR}/limit_${suffix}.log >/dev/null
+#  combine $COMBINE_ARGS ${DIR}/datacard_${mass}.txt 2>&1 | tee ${DIR}/limit_${mass}.log >/dev/null
+#  combine $COMBINE_ARGS ${DIR}/datacard_mu2jet_${mass}.txt 2>&1 | tee ${DIR}/limit_mu2jet_${mass}.log >/dev/null
+#  combine $COMBINE_ARGS ${DIR}/datacard_withVBF_${mass}.txt 2>&1 | tee ${DIR}/limit_withVBF_${mass}.log >/dev/null
+#  combine $COMBINE_ARGS ${DIR}/datacard_5pctsyst_${mass}.txt 2>&1 | tee ${DIR}/limit_5pctsyst_${mass}.log >/dev/null
+#  combine $COMBINE_ARGS ${DIR}/datacard_milwin_candc_2j_${mass}.txt 2>&1 | tee ${DIR}/limit_milwin_candc_2j_${mass}.log >/dev/null
 done
